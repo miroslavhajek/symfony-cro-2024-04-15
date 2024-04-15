3ae1ec7917d565b4910b008418f9408a7e4a3aac
Martin Hujer <mail@martinhujer.cz>
2024-04-15 13:59:31 +0200

    CarOffer

---
+++ src/CarOffer.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class CarOffer
+{
+    private string $name;
+
+    private int $price;
+
+    public function __construct(string $name, int $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+}
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\CarOffer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +13,17 @@ class HomepageController extends AbstractController
     #[Route('/', methods: ['GET'], name: 'app_homepage')]
     public function homepage(): Response
     {
+        /** @var CarOffer[] $carOffers */
+        $carOffers = [
+            new CarOffer('Škoda Octava 1.8 TDI', 199999),
+            new CarOffer('Fiat Panda', 79999),
+        ];
+
         return $this->render('homepage/homepage.html.twig', [
             'info' => 'This is a Symfony app',
             'rand' => random_int(1, 100),
             'numbers' => range(1, 10),
+            'carOffers' => $carOffers,
         ]);
     }
 

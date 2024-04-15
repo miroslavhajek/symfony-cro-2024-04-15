4fb542cf2904b22bd34fb4d468efc30ec672d2c4
Martin Hujer <mail@martinhujer.cz>
2024-04-15 14:15:40 +0200

    project structure

--- src/App/Homepage/HomepageController.php
+++ src/App/Homepage/HomepageController.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\App\Homepage;
 
-use App\CarOffer;
+use App\CarOffer\CarOffer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,13 +13,13 @@ class HomepageController extends AbstractController
     #[Route('/', methods: ['GET'], name: 'app_homepage')]
     public function homepage(): Response
     {
-        /** @var CarOffer[] $carOffers */
+        /** @var \App\CarOffer\CarOffer[] $carOffers */
         $carOffers = [
             new CarOffer('Škoda Octava 1.8 TDI', 199999),
             new CarOffer('Fiat Panda', 79999),
         ];
 
-        return $this->render('homepage/homepage.html.twig', [
+        return $this->render('@App/homepage/homepage.html.twig', [
             'info' => 'This is a Symfony app',
             'rand' => random_int(1, 100),
             'numbers' => range(1, 10),

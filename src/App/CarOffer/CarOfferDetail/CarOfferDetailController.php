a34cea545688d6f0edf3731102197be2e5bffa84
Martin Hujer <mail@martinhujer.cz>
2024-04-16 15:37:00 +0200

    CarOfferDetailController

---
+++ src/App/CarOffer/CarOfferDetail/CarOfferDetailController.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\App\CarOffer\CarOfferDetail;
+
+use App\CarOffer\CarOffer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CarOfferDetailController extends AbstractController
+{
+
+    #[Route('/nabidka/{id}', methods: ['GET'], name: 'car_offer_detail')]
+    public function __invoke(
+        CarOffer $carOffer,
+        Request $request,
+    ): Response
+    {
+        return $this->render(
+            '@App/car-offer/detail.html.twig',
+            [
+                'carOffer' => $carOffer,
+            ]
+        );
+    }
+
+}
